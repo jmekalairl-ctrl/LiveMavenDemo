2333299bd9ad9fb958f2b4d534cb68a65af46b64
mekal <JAGAN>
2026-02-18 10:33:43 +0000

    2nd Test case

--- src/test/java/testcases/AppTest1.java
+++ src/test/java/testcases/AppTest1.java
@@ -6,4 +6,17 @@ public class AppTest1
  	{
 	 System.out.println(" Hello Test class  ");
  	}
+ public void customerSale() 
+	{
+	 System.out.println(" This is a sale method  ");
+	}
+ public void orderTest() 
+	{
+	 System.out.println(" This is a order test   ");
+	}
+ public void checkoutTest() 
+	{
+	 System.out.println(" This is a checkout Method. ");
+	}
+ 
  }
